ee4d2c80c991ba109383f742de3ddb04d10d8f37
sunny-tier-6236 <taycurrie@gmail.com>
2020-04-19 00:10:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,5 @@
-require 'pry'
-
-class Author
-  attr_accessor :name
+class Song
+  attr_accessor :name, :artist
 
   @@all = []
 
@@ -14,20 +12,7 @@ class Author
     @@all
   end
 
-  def add_post(post)
-    post.author = self
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
-  end
-
-  def posts
-    Post.all.select {|post| post.author == self}
-  end
-
-  def self.post_count
-    Post.all.count
+  def artist_name
+    artist.name if artist
   end
 end
